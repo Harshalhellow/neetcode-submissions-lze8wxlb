98e7247ff34dfa4d21a586b3b132c54e11fcdddd
Harshalhellow <126425967+Harshalhellow@users.noreply.github.com>
2026-07-16 08:42:54 +0930

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b){
+            return a.start < b.start;
+        });
+        if(intervals.size()<=1) return true; 
+        for(int i=1; i<intervals.size(); i++){
+            if(intervals[i-1].end>intervals[i].start) return false; 
+        } 
+        return true;
+    }
+};
+
+
+
